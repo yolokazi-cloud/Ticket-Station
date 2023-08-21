f6d64794e36d5352a62a2fd8c42e33a670028849
Bongz Podolski <ntshingilab@outlook.com>
2023-08-21 06:02:49 +0200

    login screen added

---
+++ login.js
@@ -0,0 +1,55 @@
+class User {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+document.getElementById("signInForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+
+  const usernameInput = document.querySelector('input[name="txt"]');
+  const emailInput = document.querySelector('input[name="email"]');
+  const passwordInput = document.querySelector('input[name="pswd"]');
+  const username = usernameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+ 
+  if (username.trim() === '' || email.trim() === '' || password.trim() === '') {
+    alert('Please enter all required fields.');
+    return;
+  }
+
+
+  if (!isValidEmail(email)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+
+  if (!isPasswordValid(password)) {
+    alert('Password should have at least 4 characters,and a number');
+    return;
+  }
+
+
+  const user = new User(username, email, password);
+
+  sessionStorage.setItem('user', JSON.stringify(user));
+
+
+  window.location.href = "Events.html";
+});
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+
+function isPasswordValid(password){
+   const passwordRegex = /^(?=.*[a-z])(?=.*\d).{4,}$/;
+  return passwordRegex.test(password);
+}
